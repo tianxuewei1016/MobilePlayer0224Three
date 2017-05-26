ece92e611329fb8cdfeb7c8fbf6ade8fd2b6163c
tianxuewei1016 <93226539@qq.com>
2017-05-26 20:21:38 +0800

    音乐的暂停和播放

--- app/src/main/java/mobilevideo0224/mobilevideo0224/activity/SystemAudioPlayerActivity.java
+++ app/src/main/java/mobilevideo0224/mobilevideo0224/activity/SystemAudioPlayerActivity.java
@@ -1,13 +1,26 @@
 package mobilevideo0224.mobilevideo0224.activity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
+import mobilevideo0224.mobilevideo0224.IMusicPlayService;
 import mobilevideo0224.mobilevideo0224.R;
 import mobilevideo0224.mobilevideo0224.service.MusicPlayService;
 
@@ -15,17 +28,120 @@ public class SystemAudioPlayerActivity extends AppCompatActivity {
 
     @InjectView(R.id.iv_icon)
     ImageView ivIcon;
+    @InjectView(R.id.tv_artist)
+    TextView tvArtist;
+    @InjectView(R.id.tv_audioname)
+    TextView tvAudioname;
+    @InjectView(R.id.rl_top)
+    RelativeLayout rlTop;
+    @InjectView(R.id.tv_time)
+    TextView tvTime;
+    @InjectView(R.id.seekbar_audio)
+    SeekBar seekbarAudio;
+    @InjectView(R.id.btn_playmode)
+    Button btnPlaymode;
+    @InjectView(R.id.btn_pre)
+    Button btnPre;
+    @InjectView(R.id.btn_start_pause)
+    Button btnStartPause;
+    @InjectView(R.id.btn_next)
+    Button btnNext;
+    @InjectView(R.id.btn_lyric)
+    Button btnLyric;
+    @InjectView(R.id.ll_bottom)
+    LinearLayout llBottom;
+    //这个就是IMusicPlayService.Stub的实例
+    private IMusicPlayService service;
+    private int position;
+    private ServiceConnection conon = new ServiceConnection() {
+        /**
+         * 当绑定服务成功的时候回调
+         * @param name
+         * @param iBinder    就是IMusicPlayService.Stub的实例
+         */
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder iBinder ) {
+            //这个就是stub，stub包含很多方法，这些方法调用服务的方法
+            service = IMusicPlayService.Stub.asInterface(iBinder);
+            try {
+                service.openAudio(position);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * 当断开连接的时候
+         * @param name
+         */
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_system_audio_player);
         ButterKnife.inject(this);
+
+
         ivIcon.setBackgroundResource(R.drawable.animation_bg);
         AnimationDrawable drawable = (AnimationDrawable) ivIcon.getBackground();
         drawable.start();
+        getData();
+        startAndBindService();
+    }
 
+    private void startAndBindService() {
         Intent intent = new Intent(this, MusicPlayService.class);
+        //绑定-得到服务的操作对象-IMusicPlayService service
+        bindService(intent,conon, Context.BIND_AUTO_CREATE);
+        //防止多次实例化Service
         startService(intent);
+    }
+
+    private void getData() {
+        position = getIntent().getIntExtra("position",0);
+    }
+
+    @OnClick({R.id.btn_playmode, R.id.btn_pre, R.id.btn_start_pause, R.id.btn_next, R.id.btn_lyric})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_playmode:
+                break;
+            case R.id.btn_pre:
+                break;
+            case R.id.btn_start_pause:
+                try {
+                    if(service.isPlaying()) {
+                        //暂停
+                        service.pause();
+                        //按钮状态--播放
+                        btnStartPause.setBackgroundResource(R.drawable.btn_audio_start_selector);
+                    }else {
+                        //播放
+                        service.start();
+                        //按钮状态--暂停
+                        btnStartPause.setBackgroundResource(R.drawable.btn_audio_pause_selector);
+                    }
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.btn_next:
+                break;
+            case R.id.btn_lyric:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(conon != null) {
+            unbindService(conon);
+        }
     }
 }
--- app/src/main/java/mobilevideo0224/mobilevideo0224/service/MusicPlayService.java
+++ app/src/main/java/mobilevideo0224/mobilevideo0224/service/MusicPlayService.java
@@ -4,11 +4,13 @@ import android.app.Service;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -81,13 +83,28 @@ public class MusicPlayService extends Service {
         public void pre() throws RemoteException {
             service.pre();
         }
+
+        @Override
+        public boolean isPlaying() throws RemoteException {
+            return mediaPlayer.isPlaying();
+        }
     };
 
     private ArrayList<MediaItem> mediaItems;
+    private MediaPlayer mediaPlayer;
+    /**
+     * 音频列表的下标位置
+     */
+    private int position;
+    /**
+     * 代表一个音频信息类
+     */
+    private MediaItem mediaItem;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.e("TAG", "MusicPlayService--onCreate()");
         //加载列表数据
         getData();
     }
@@ -138,20 +155,72 @@ public class MusicPlayService extends Service {
      * @param position
      */
     private void openAudio(int position) {
+        this.position = position;
+        if (mediaItems != null && mediaItems.size() > 0) {
+            if (position < mediaItems.size()) {
+                mediaItem = mediaItems.get(position);
+                //如果不为空释放之前的播放音频的资源
+                if (mediaPlayer != null) {
+                    mediaPlayer.reset();
+                    mediaPlayer = null;
+                }
+                try {
+                    mediaPlayer = new MediaPlayer();
+                    //设置播放地址
+                    mediaPlayer.setDataSource(mediaItem.getData());
+                    //设置最基本的三个监听：准备完成，播放出错，播放完成
+                    mediaPlayer.setOnPreparedListener(new MyOnPreparedListener());
+                    mediaPlayer.setOnErrorListener(new MyOnErrorListener());
+                    mediaPlayer.setOnCompletionListener(new MyOnCompletionListener());
+                    //准备
+                    mediaPlayer.prepareAsync();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            Toast.makeText(MusicPlayService.this, "音频还没有加载完成", Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    class MyOnPreparedListener implements MediaPlayer.OnPreparedListener {
+
+        @Override
+        public void onPrepared(MediaPlayer mp) {
+            start();
+        }
+    }
+
+    class MyOnErrorListener implements MediaPlayer.OnErrorListener {
+
+        @Override
+        public boolean onError(MediaPlayer mp, int what, int extra) {
+            next();//播放下一个
+            return true;
+        }
+    }
+
+    class MyOnCompletionListener implements MediaPlayer.OnCompletionListener {
+
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            //播放下一个
+            next();
+        }
     }
 
     /**
      * 播放音频
      */
     private void start() {
+        mediaPlayer.start();//开始播放
     }
 
     /**
      * 暂停音频
      */
     private void pause() {
-
+        mediaPlayer.pause();
     }
 
     /**
