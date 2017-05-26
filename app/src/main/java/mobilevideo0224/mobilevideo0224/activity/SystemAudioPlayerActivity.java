8de1e4a497b2d6d18c8837b23f3f34f1cffccb6c
tianxuewei1016 <93226539@qq.com>
2017-05-26 19:31:13 +0800

    音乐播放器页面-动画列表-帧动画

--- app/src/main/java/mobilevideo0224/mobilevideo0224/activity/SystemAudioPlayerActivity.java
+++ app/src/main/java/mobilevideo0224/mobilevideo0224/activity/SystemAudioPlayerActivity.java
@@ -1,15 +1,26 @@
 package mobilevideo0224.mobilevideo0224.activity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import mobilevideo0224.mobilevideo0224.R;
 
 public class SystemAudioPlayerActivity extends AppCompatActivity {
+
+    @InjectView(R.id.iv_icon)
+    ImageView ivIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_system_audio_player);
+        ButterKnife.inject(this);
+        ivIcon.setBackgroundResource(R.drawable.animation_bg);
+        AnimationDrawable drawable = (AnimationDrawable) ivIcon.getBackground();
+        drawable.start();
     }
 }
